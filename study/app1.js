9ff55787dcaa7e5abba58dc1779f05e8c07aca0d
Kyo Choi <48676739+Choisk2003@users.noreply.github.com>
2020-03-06 19:41:36 +0900

    Add checking undefined

--- study/app1.js
+++ study/app1.js
@@ -5,13 +5,36 @@ var url = require("url");
 var app = http.createServer(function(request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
-  var title = queryData.id;
-  if (_url == "/") {
-    title = "Welcome";
-  }
-  response.writeHead(200);
-  fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
-    var template = `
+  var pathname = url.parse(_url, true).pathname;
+
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      var title = "Welcome";
+      var description = "Hello World";
+      var template = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+      <meta charset="UTF-8">
+      <title>${title}</title>
+  </head>
+  <body>
+      <h1><a href="/">Welcome</a></h1>
+      <ul>
+        <li><a href="/?id=me">Me</a></li>
+        <li><a href="/?id=you">You</a></li>
+        <li><a href="/?id=us">Us</a></li>
+      </ul>
+      <h2>${title}</h2>
+      <p>${description}</p>
+  </body>
+  </html>`;
+      response.writeHead(200);
+      response.end(template);
+    } else {
+      fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
+        var title = queryData.id;
+        var template = `
   <!DOCTYPE html>
   <html>
   <head>
@@ -29,8 +52,14 @@ var app = http.createServer(function(request, response) {
       <p>${description}</p>
   </body>
   </html>`;
-    response.end(template);
-  });
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+  } else {
+    response.writeHead(404);
+    response.end("Not Found");
+  }
 });
 
 app.listen(3000);
