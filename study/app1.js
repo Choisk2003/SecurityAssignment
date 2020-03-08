1e9c2c24af38fdf18435d4b3d50f7d3cda1e1903
Kyo Choi <48676739+Choisk2003@users.noreply.github.com>
2020-03-08 15:00:42 +0900

    Add check dir

--- study/app1.js
+++ study/app1.js
@@ -9,51 +9,62 @@ var app = http.createServer(function(request, response) {
 
   if (pathname === "/") {
     if (queryData.id === undefined) {
-      var title = "Welcome";
-      var description = "Hello World";
-      var template = `
-  <!DOCTYPE html>
-  <html>
-  <head>
-      <meta charset="UTF-8">
-      <title>${title}</title>
-  </head>
-  <body>
-      <h1><a href="/">Welcome</a></h1>
-      <ul>
-        <li><a href="/?id=me">Me</a></li>
-        <li><a href="/?id=you">You</a></li>
-        <li><a href="/?id=us">Us</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>${description}</p>
-  </body>
-  </html>`;
-      response.writeHead(200);
-      response.end(template);
-    } else {
-      fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
-        var title = queryData.id;
+      fs.readdir("./data", function(error, filelist) {
+        var title = "Welcome";
+        var description = "Hello World";
+
+        var list = "<ul>";
+        for (let i = 0; i < filelist.length; i++) {
+          list =
+            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        }
+        list = list + "</ul>";
+
         var template = `
-  <!DOCTYPE html>
-  <html>
-  <head>
-      <meta charset="UTF-8">
-      <title>${title}</title>
-  </head>
-  <body>
-      <h1><a href="/">Welcome</a></h1>
-      <ul>
-        <li><a href="/?id=me">Me</a></li>
-        <li><a href="/?id=you">You</a></li>
-        <li><a href="/?id=us">Us</a></li>
-      </ul>
-      <h2>It's about ${title}</h2>
-      <p>${description}</p>
-  </body>
-  </html>`;
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <meta charset="UTF-8">
+          <title>${title}</title>
+        </head>
+        <body>
+          <h1><a href="/">Welcome</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>`;
         response.writeHead(200);
         response.end(template);
+      });
+    } else {
+      fs.readdir("./data", function(error, filelist) {
+        var list = "<ul>";
+        for (let i = 0; i < filelist.length; i++) {
+          list =
+            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        }
+        list = list + "</ul>";
+
+        fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
+          var title = queryData.id;
+          var template = `
+          <!DOCTYPE html>
+          <html>
+          <head>
+              <meta charset="UTF-8">
+              <title>${title}</title>
+          </head>
+          <body>
+              <h1><a href="/">Welcome</a></h1>
+              ${list}
+              <h2>It's about ${title}</h2>
+              <p>${description}</p>
+          </body>
+          </html>`;
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
