fc034f7883647e2bfa303ea4f49264e2446ebe16
Kyo Choi <48676739+Choisk2003@users.noreply.github.com>
2020-03-09 20:40:18 +0900

    clean up code with function

--- study/app1.js
+++ study/app1.js
@@ -2,6 +2,32 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 
+function templateMain(title, description, list) {
+  return `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="UTF-8">
+    <title>${title}</title>
+  </head>
+  <body>
+    <h1><a href="/">Welcome</a></h1>
+    ${list}
+    <h2>${title}</h2>
+    <p>${description}</p>
+  </body>
+  </html>`;
+}
+
+function templateList(filelist) {
+  var list = "<ul>";
+  for (let i = 0; i < filelist.length; i++) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+  }
+  list = list + "</ul>";
+  return list;
+}
+
 var app = http.createServer(function(request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
@@ -13,55 +39,22 @@ var app = http.createServer(function(request, response) {
         var title = "Welcome";
         var description = "Hello World";
 
-        var list = "<ul>";
-        for (let i = 0; i < filelist.length; i++) {
-          list =
-            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        }
-        list = list + "</ul>";
+        var template = templateMain(title, description, templateList(filelist));
 
-        var template = `
-        <!DOCTYPE html>
-        <html>
-        <head>
-          <meta charset="UTF-8">
-          <title>${title}</title>
-        </head>
-        <body>
-          <h1><a href="/">Welcome</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>`;
         response.writeHead(200);
         response.end(template);
       });
     } else {
       fs.readdir("./data", function(error, filelist) {
-        var list = "<ul>";
-        for (let i = 0; i < filelist.length; i++) {
-          list =
-            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        }
-        list = list + "</ul>";
-
         fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
           var title = queryData.id;
-          var template = `
-          <!DOCTYPE html>
-          <html>
-          <head>
-              <meta charset="UTF-8">
-              <title>${title}</title>
-          </head>
-          <body>
-              <h1><a href="/">Welcome</a></h1>
-              ${list}
-              <h2>It's about ${title}</h2>
-              <p>${description}</p>
-          </body>
-          </html>`;
+
+          var template = templateMain(
+            title,
+            description,
+            templateList(filelist)
+          );
+
           response.writeHead(200);
           response.end(template);
         });
