f8cca9a0164800dbd066a2750e9d1a939cb427bc
Kyo Choi <48676739+Choisk2003@users.noreply.github.com>
2020-04-08 16:37:55 +0900

    Connect db

--- project/app.js
+++ project/app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const router = require("./routes/index");
 
-const signinRoute = require("./routes/signin");
+const db = mongoose.connection;
+db.on("error", console.error);
+db.once("open", function () {
+  console.log("Connected to mongod server");
+});
+mongoose.connect("mongodb://localhost:27017/AfterReading");
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use("/", signinRoute);
+app.use("/", router);
 
 app.listen(3000);
