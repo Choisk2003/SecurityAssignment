40df46b5ae1408c5ba8bafbf24fe1125de067334
Kyo Choi <48676739+Choisk2003@users.noreply.github.com>
2020-04-09 19:03:58 +0900

    email regular expression

--- project/routes/index.js
+++ project/routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const session = require("express-session");
 const router = express.Router();
 
 router.get("/", (req, res) => res.render("signin"));
 router.post("/", (req, res) => res.render("signin"));
 
-router.get("/signup", (req, res) => res.render("signup"));
+router.get("/signup", (req, res) => res.render("signup", { message: null }));
 router.post("/signup", (req, res) => {
   require("../api/signup")(req, res);
 });
