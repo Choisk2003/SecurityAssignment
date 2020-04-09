11db4aa6d5e89e73ac4d4a6dc60efcee3bc59622
Kyo Choi <48676739+Choisk2003@users.noreply.github.com>
2020-04-09 19:02:51 +0900

    Update app.js
    add email reqular expresion

--- project/api/signup.js
+++ project/api/signup.js
@@ -5,20 +5,25 @@ const users = mongoose.model("users", userSchema);
 
 module.exports = async function (req, res) {
   userInfo = req.body;
-  const findByEmail = await users.findOne({ email: userInfo.email }); //정규표현식으로 확인 필요
-  const findById = await users.findOne({ userId: userInfo.userid });
-  if (findByEmail === null && findById === null) {
-    users
-      .create({
-        email: userInfo.email,
-        userName: userInfo.username,
-        userId: userInfo.userid,
-        password: userInfo.password,
-        reports: [],
-      })
-      .then(() => res.end("well done"));
+  const emailPattern = /.+@[0-9a-z\-]+\.[a-z]+/i;
+  if (emailPattern.test(userInfo.email)) {
+    const findByEmail = await users.findOne({ email: userInfo.email });
+    const findById = await users.findOne({ userId: userInfo.userid });
+    if (findByEmail === null && findById === null) {
+      users
+        .create({
+          email: userInfo.email,
+          userName: userInfo.username,
+          userId: userInfo.userid,
+          password: userInfo.password,
+          reports: [],
+        })
+        .then(() => res.end("well done"));
+    } else {
+      console.log("already exist");
+      res.end("already exist");
+    }
   } else {
-    console.log("already exist");
-    res.end("already exist");
+    res.render("signup", { message: "잘못된 이메일 형식입니다." });
   }
 };
