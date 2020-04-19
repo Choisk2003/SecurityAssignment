7ff1b0574395648b11f33dc467d7a20604e719f0
Kyo Choi <48676739+Choisk2003@users.noreply.github.com>
2020-04-19 15:30:01 +0900

    Update app.js
    Add session and mongo session store

--- project/app.js
+++ project/app.js
@@ -2,21 +2,37 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const router = require("./routes/index");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const accountRouter = require("./routes/account");
+const indexRouter = require("./routes/index");
 
+const dbUrl = "mongodb://localhost:27017/AfterReading";
 const db = mongoose.connection;
 db.on("error", console.error);
 db.once("open", function () {
   console.log("Connected to mongod server");
 });
-mongoose.connect("mongodb://localhost:27017/AfterReading");
+mongoose.connect(dbUrl);
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(
+  session({
+    secret: "1p2o3i",
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({
+      url: dbUrl,
+      collection: "sessions",
+    }),
+  })
+);
 
-app.use("/", router);
+app.use("/", indexRouter);
+app.use("/sign", accountRouter);
 
 app.listen(3000);
