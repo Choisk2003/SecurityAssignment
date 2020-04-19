49ab3c46764addf6a33e3fae7980ae9df82ee97e
Kyo Choi <48676739+Choisk2003@users.noreply.github.com>
2020-04-19 15:29:21 +0900

    Update signup.js
    made it work as i want

--- project/api/signup.js
+++ project/api/signup.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
 const userSchema = require("../model/userSchema");
 const users = mongoose.model("users", userSchema);
 
 module.exports = async function (req, res) {
-  userInfo = req.body;
+  const userInfo = req.body;
   const emailPattern = /.+@[0-9a-z\-]+\.[a-z]+/i;
   if (emailPattern.test(userInfo.email)) {
     const findByEmail = await users.findOne({ email: userInfo.email });
@@ -18,10 +17,11 @@ module.exports = async function (req, res) {
           password: userInfo.password,
           reports: [],
         })
-        .then(() => res.end("well done"));
+        .then(() => res.redirect("/sign"));
     } else {
-      console.log("already exist");
-      res.end("already exist");
+      res.render("signup", {
+        message: "이미 가입된 이메일 혹은 아이디 입니다.",
+      });
     }
   } else {
     res.render("signup", { message: "잘못된 이메일 형식입니다." });
